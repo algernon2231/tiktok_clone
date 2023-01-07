2a7e1b65d1d453312602bf06dd02de28f5c41867
algernon2231 <39697959+algernon2231@users.noreply.github.com>
2023-01-07 20:53:39 +0700

    hj

---
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+const Sidebar = () => {
+  return (
+   <h2>Sidebar</h2>
+  )
+}
+
+export default Sidebar
---
+++ src/pages/Upload/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Upload = () => {
+  return (
+    <>
+         <h2>Upload Page</h2>
+    </>
+  )
+}
+
+export default Upload
